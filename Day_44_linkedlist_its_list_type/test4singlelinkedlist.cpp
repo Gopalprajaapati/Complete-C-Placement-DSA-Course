d39da36536897e8cc8e4ae48e54a73a76918488a
gopal25119 <gopalprajapati25119@gmail.com>
2024-11-18 17:17:02 +0530

    test single list

--- Day_44_linkedlist_its_list_type/test4singlelinkedlist.cpp
+++ Day_44_linkedlist_its_list_type/test4singlelinkedlist.cpp
@@ -3,68 +3,121 @@ using namespace std;
 
 class Node
 {
-
 public:
     int data;
-    Node* next;
+    Node *next;
     Node(int data)
     {
         this->data = data;
         this->next = NULL;
     }
-    ~Node()
-    {
-        int val = this->data;
-        if (this->next = NULL)
-        {
-            delete next;
-        }
-    }
 };
 
-void insertAtHead(Node *&head, int d)
+void insertAthead(Node *&head, Node *&tail, int d)
 {
-   
-    
     Node *temp = new Node(d);
-    temp->next = head;
-    head = temp;
+    if (head == NULL)
+    {
+        head = temp;
+        tail = temp;
+    }
+    else
+    {
+        temp->next = head;
+        head = temp;
+    }
 }
-void print(Node* &head)
+
+void insertAttail(Node *&tail, Node *&head, int d)
+{
+    Node *temp = new Node(d);
+    if (tail == NULL)
+    {
+        head = temp;
+        tail = temp;
+    }
+    else
+    {
+        tail->next = temp; // Append the new node at the end
+        tail = temp;       // Update the tail pointer
+    }
+}
+
+void print(Node *&head)
 {
     Node *temp = head;
 
     while (temp != NULL)
     {
         cout << temp->data << " ";
-       temp = temp->next;
+        temp = temp->next;
     }
     cout << endl;
 }
-void insertAtTail(Node* &tail, int d)
+
+void insertAtPosition(Node *&head, Node *&tail, int position, int d)
 {
-    Node *temp = new Node(d);
-    tail->next = temp;
-    tail = temp;
+    // Insert at the head
+    if (position == 1)
+    {
+        insertAthead(head, tail, d);
+        return;
+    }
+
+    Node *temp = head;
+    int cnt = 1;
+
+    // Traverse to the position before the desired index
+    while (cnt < position - 1 && temp != NULL)
+    {
+        temp = temp->next;
+        cnt++;
+    }
+
+    // If inserting at the end or beyond
+    if (temp == NULL || temp->next == NULL)
+    {
+        insertAttail(tail, head, d);
+        return;
+    }
+
+    // Insert in the middle
+    Node *nodeIndex = new Node(d);
+    nodeIndex->next = temp->next;
+    temp->next = nodeIndex;
 }
 
 int main()
 {
-    cout << "single linkedlist node code ";
-    Node *node1 = new Node(10);
-    cout << node1->data;
-    Node *head = node1;
-    Node *tail = node1;
-    insertAtHead(head, 20);
-    insertAtHead(head, 30);
-    insertAtHead(head, 40);
-    insertAtHead(head, 50);
-   
+    Node *head = NULL;
+    Node *tail = NULL;
 
-    insertAtTail(tail, 20);
-    insertAtTail(tail, 30);
-    insertAtTail(tail, 40);
-    insertAtTail(tail, 50);
+    insertAthead(head, tail, 10);
     print(head);
 
+    insertAthead(head, tail, 20);
+    print(head);
+
+    insertAthead(head, tail, 30);
+    print(head);
+
+    insertAttail(tail, head, 90);
+    print(head);
+
+    insertAttail(tail, head, 100);
+    print(head);
+
+    insertAttail(tail, head, 120);
+    print(head);
+
+    insertAtPosition(head, tail, 2, 65);
+    print(head);
+
+    insertAtPosition(head, tail, 1, 5);
+    print(head);
+
+    insertAtPosition(head, tail, 10, 150);
+    print(head);
+
+    return 0;
 }
