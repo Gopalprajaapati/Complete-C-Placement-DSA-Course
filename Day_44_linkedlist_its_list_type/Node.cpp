767e63d85b936297997b433965d24f1e5395761c
gopal25119 <gopalprajapati25119@gmail.com>
2024-12-26 17:20:03 +0530

    double linked list code

--- Day_44_linkedlist_its_list_type/Node.cpp
+++ Day_44_linkedlist_its_list_type/Node.cpp
@@ -1,184 +1,173 @@
 #include <iostream>
 using namespace std;
 
-class Node
-{
+class Node {
 public:
     int data;
-    Node *next;
+    Node* next;
+    Node* prev;
 
-    Node(int data)
-    {
+    Node(int data) {
         this->data = data;
         this->next = NULL;
+        this->prev = NULL;
     }
 };
 
-void insertAtHead(Node *&head, Node *&tail, int d)
-{
-    Node *temp = new Node(d);
-    if (head == NULL)
-    {
+void insertAtHead(Node*& head, Node*& tail, int d) {
+    Node* temp = new Node(d);
+    if (head == NULL) {
         head = temp;
         tail = temp;
         return;
     }
     temp->next = head;
+    head->prev = temp;
     head = temp;
 }
 
-void insertAtTail(Node *&head, Node *&tail, int d)
-{
-    Node *temp = new Node(d);
-    if (tail == NULL)
-    {
+void insertAtTail(Node*& head, Node*& tail, int d) {
+    Node* temp = new Node(d);
+    if (tail == NULL) {
         head = temp;
         tail = temp;
         return;
     }
     tail->next = temp;
+    temp->prev = tail;
     tail = temp;
 }
 
-void insertAtPosition(Node *&head, Node *&tail, int position, int d)
-{
-    if (position == 1)
-    {
+void insertAtPosition(Node*& head, Node*& tail, int position, int d) {
+    if (position == 1) {
         insertAtHead(head, tail, d);
         return;
     }
-    Node *temp = head;
+
+    Node* temp = head;
     int cnt = 1;
-    while (cnt < position - 1 && temp != NULL)
-    {
+    while (cnt < position - 1 && temp != NULL) {
         temp = temp->next;
         cnt++;
     }
-    if (temp == NULL)
-    {
-        cout << "linkedlist is empty" << endl;
+
+    if (temp == NULL) {
+        cout << "Position out of bounds. Inserting at the end." << endl;
+        insertAtTail(head, tail, d);
         return;
     }
 
-    Node *nodeIndex = new Node(d);
+    Node* nodeIndex = new Node(d);
     nodeIndex->next = temp->next;
-    temp->next = nodeIndex;
-
-    if (nodeIndex->next == NULL)
-    {
+    nodeIndex->prev = temp;
+    if (temp->next != NULL) {
+        temp->next->prev = nodeIndex;
+    } else {
         tail = nodeIndex;
-        return;
     }
+    temp->next = nodeIndex;
 }
 
-void print(Node *head)
-{
-    Node *temp = head;
+void deleteByPosition(Node*& head, Node*& tail, int position) {
+    if (head == NULL) {
+        cout << "List is empty!" << endl;
+        return;
+    }
+
+    if (position == 1) {
+        Node* temp = head;
+        head = head->next;
+        if (head != NULL) {
+            head->prev = NULL;
+        } else {
+            tail = NULL;
+        }
+        delete temp;
+        return;
+    }
+
+    Node* curr = head;
     int cnt = 1;
-    while (temp != NULL)
-    {
+    while (cnt < position && curr != NULL) {
+        curr = curr->next;
+        cnt++;
+    }
+
+    if (curr == NULL) {
+        cout << "Position out of bounds!" << endl;
+        return;
+    }
+
+    if (curr->next != NULL) {
+        curr->next->prev = curr->prev;
+    } else {
+        tail = curr->prev;
+    }
+
+    curr->prev->next = curr->next;
+    delete curr;
+}
+
+void deleteByValue(Node*& head, Node*& tail, int value) {
+    if (head == NULL) {
+        cout << "List is empty!" << endl;
+        return;
+    }
+
+    if (head->data == value) {
+        Node* temp = head;
+        head = head->next;
+        if (head != NULL) {
+            head->prev = NULL;
+        } else {
+            tail = NULL;
+        }
+        delete temp;
+        return;
+    }
+
+    Node* curr = head;
+    while (curr != NULL && curr->data != value) {
+        curr = curr->next;
+    }
+
+    if (curr == NULL) {
+        cout << "Value not found in the list!" << endl;
+        return;
+    }
+
+    if (curr->next != NULL) {
+        curr->next->prev = curr->prev;
+    } else {
+        tail = curr->prev;
+    }
+
+    curr->prev->next = curr->next;
+    delete curr;
+}
+
+void print(Node* head) {
+    Node* temp = head;
+    while (temp != NULL) {
         cout << temp->data << " ";
         temp = temp->next;
-        cnt++;
     }
     cout << endl;
 }
 
-void deleteByPosition(Node *&head, Node *&tail, int position)
-{
-    if (head == NULL)
-    {
-        cout << "linkedlist is empty:";
-        return;
+void printReverse(Node* tail) {
+    Node* temp = tail;
+    while (temp != NULL) {
+        cout << temp->data << " ";
+        temp = temp->prev;
     }
-
-    if (position == 1)
-    {
-        Node *temp = head;
-        head = head->next;
-        delete temp;
-        if (head == NULL)
-        {
-
-            tail = NULL;
-        }
-        return;
-    }
-
-    Node *curr = head;
-    Node *prev = NULL;
-    int cnt = 1;
-    while (cnt < position && curr != NULL)
-    {
-        prev = curr;
-        curr = curr->next;
-        cnt++;
-    }
-    if (curr == NULL)
-    {
-        cout << "Position is out of bounds." << endl;
-        return;
-    }
-
-    prev->next = curr->next;
-
-    if (curr->next == NULL)
-    {
-        tail = prev;
-    }
-    delete curr;
+    cout << endl;
 }
 
-void deleteByValue(Node *&head, Node *&tail, int value)
-{
-    if (head ==NULL)
-    {
-       cout << "Linked list empty";
-       return;
-    }
-    if (head->data == value)
-    {
-        Node *temp = head;
-        head = head->next;
-        delete temp;
-        if (head == NULL)
-        {
-            tail = NULL;
-        }
-        return;
-    }
+int main() {
+    Node* head = NULL;
+    Node* tail = NULL;
 
-    Node* curr= head;
-    Node* prev = NULL;
-    int cnt =1;
-    while (curr != NULL && curr->data != value)
-    {
-       prev = curr;
-       curr = curr->next;
-       cnt++;
-    }
-
-    if (curr == NULL)
-    {
-        cout << "LInked lis bound";
-        return;
-    }
-    prev->next = curr->next;
-    if (curr->next == NULL)
-    {
-        tail = prev;
-        return;
-    }
-    delete curr;
-    
-
-    
-}
-
-int main()
-{
-    Node *head = NULL;
-    Node *tail = NULL;
     // Insert nodes
     insertAtHead(head, tail, 10);
     insertAtHead(head, tail, 20);
@@ -189,25 +178,36 @@ int main()
     insertAtPosition(head, tail, 5, 200); // Insert in the middle
     insertAtPosition(head, tail, 8, 300); // Insert at tail
 
+    cout << "Doubly Linked List: ";
     print(head);
+
+    cout << "Reverse: ";
+    printReverse(tail);
 
     // Delete nodes
     deleteByPosition(head, tail, 5); // Delete node at position 5
+    cout << "After deleting at position 5: ";
     print(head);
 
     deleteByPosition(head, tail, 1); // Delete the head
+    cout << "After deleting the head: ";
     print(head);
 
     deleteByPosition(head, tail, 7); // Delete the tail
-    print(head); 
-       // Delete node by value
+    cout << "After deleting the tail: ";
+    print(head);
+
+    // Delete by value
     deleteByValue(head, tail, 20); // Delete node with value 20
+    cout << "After deleting value 20: ";
     print(head);
 
     deleteByValue(head, tail, 300); // Delete node with value 300
+    cout << "After deleting value 300: ";
     print(head);
 
     deleteByValue(head, tail, 1000); // Attempt to delete a non-existent value
+    cout << "After attempting to delete non-existent value: ";
     print(head);
 
     return 0;
--- Day_44_linkedlist_its_list_type/doublelist.cpp
+++ Day_44_linkedlist_its_list_type/doublelist.cpp
@@ -1,78 +1,181 @@
 #include <iostream>
 using namespace std;
 
-class Node
-{
+class Node {
 public:
     int data;
-    Node *next;
-    Node *prev; // Added for doubly linked list
+    Node* next;
+    Node* prev;
 
-    Node(int data)
-    {
+    Node(int data) {
         this->data = data;
         this->next = NULL;
         this->prev = NULL;
     }
 };
 
-void insertAthead(Node *&head, Node *&tail, int d)
-{
-    Node *temp = new Node(d);
-    if (head == NULL)
-    {
-        head = temp;
-        tail = temp;
-        return;
+class DoublyLinkedList {
+private:
+    Node* head;
+    Node* tail;
+
+public:
+    DoublyLinkedList() {
+        head = NULL;
+        tail = NULL;
     }
 
-    // Update pointers to insert at head
-    temp->next = head;
-    head->prev = temp;
+    void insertAtHead(int data) {
+        Node* newNode = new Node(data);
+        if (head == NULL) {
+            head = newNode;
+            tail = newNode;
+            return;
+        }
 
-    // Move head to the new node
-    head = temp;
-}
-
-
-
-void print(Node *head)
-{
-    Node *temp = head;
-
-    while (temp != NULL)
-    {
-        cout << temp->data << " ";
-        temp = temp->next;
+        newNode->next = head;
+        head->prev = newNode;
+        head = newNode;
     }
-    cout << endl;
-}
 
-int length(Node *head)
-{
-    Node *temp = head;
-    int count = 0;
-    while (temp != NULL)
-    {
-        count++;
-        temp = temp->next;
+    void insertAtTail(int data) {
+        Node* newNode = new Node(data);
+        if (tail == NULL) {
+            head = newNode;
+            tail = newNode;
+            return;
+        }
+
+        tail->next = newNode;
+        newNode->prev = tail;
+        tail = newNode;
     }
-    return count;
-}
 
-int main()
-{
-    Node *head = NULL;
-    Node *tail = NULL;
+    void insertAtPosition(int position, int data) {
+        if (position == 1) {
+            insertAtHead(data);
+            return;
+        }
 
-    insertAthead(head, tail, 10);
-    insertAthead(head, tail, 20);
-    insertAthead(head, tail, 30);
+        Node* temp = head;
+        int count = 1;
 
-    print(head);
+        while (temp != NULL && count < position - 1) {
+            temp = temp->next;
+            count++;
+        }
 
-    // Print the length of the list
-    cout << "Length of the list: " << length(head) << endl;
+        if (temp == NULL) {
+            insertAtTail(data);
+            return;
+        }
+
+        Node* newNode = new Node(data);
+        newNode->next = temp->next;
+        if (temp->next != NULL) {
+            temp->next->prev = newNode;
+        }
+        temp->next = newNode;
+        newNode->prev = temp;
+
+        if (newNode->next == NULL) {
+            tail = newNode;
+        }
+    }
+
+    void deleteAtPosition(int position) {
+        if (head == NULL) {
+            cout << "List is empty!" << endl;
+            return;
+        }
+
+        if (position == 1) {
+            Node* temp = head;
+            head = head->next;
+            if (head != NULL) {
+                head->prev = NULL;
+            } else {
+                tail = NULL; // If list becomes empty
+            }
+            delete temp;
+            return;
+        }
+
+        Node* temp = head;
+        int count = 1;
+
+        while (temp != NULL && count < position) {
+            temp = temp->next;
+            count++;
+        }
+
+        if (temp == NULL) {
+            cout << "Position out of bounds!" << endl;
+            return;
+        }
+
+        if (temp->next != NULL) {
+            temp->next->prev = temp->prev;
+        } else {
+            tail = temp->prev; // If deleting the tail
+        }
+
+        if (temp->prev != NULL) {
+            temp->prev->next = temp->next;
+        }
+
+        delete temp;
+    }
+
+    void print() {
+        Node* temp = head;
+        while (temp != NULL) {
+            cout << temp->data << " ";
+            temp = temp->next;
+        }
+        cout << endl;
+    }
+
+    void printReverse() {
+        Node* temp = tail;
+        while (temp != NULL) {
+            cout << temp->data << " ";
+            temp = temp->prev;
+        }
+        cout << endl;
+    }
+
+    int length() {
+        int count = 0;
+        Node* temp = head;
+        while (temp != NULL) {
+            count++;
+            temp = temp->next;
+        }
+        return count;
+    }
+};
+
+int main() {
+    DoublyLinkedList dll;
+
+    dll.insertAtHead(10);
+    dll.insertAtHead(20);
+    dll.insertAtTail(30);
+    dll.insertAtTail(40);
+    dll.insertAtPosition(3, 25); // Insert 25 at position 3
+
+    cout << "Doubly Linked List: ";
+    dll.print();
+
+    cout << "Reverse: ";
+    dll.printReverse();
+
+    cout << "Length: " << dll.length() << endl;
+
+    dll.deleteAtPosition(3); // Delete node at position 3
+    cout << "After Deletion at Position 3: ";
+    dll.print();
 
     return 0;
 }
